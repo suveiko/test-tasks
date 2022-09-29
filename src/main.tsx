87b2b4f71975e9dd9372726548451c058b5fb2d5
suveiko <suveikosasha@gmail.com>
2022-09-30 01:40:22 +0400

    feat: complete third task

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+import './1task-HttpRequestHandling';
+import './2task-Counter/index';
+import './2task-Counter/counter-test';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<h1>Test</h1>);
